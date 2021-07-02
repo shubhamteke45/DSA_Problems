0029e2f43a300cd24586b1dbaf4874330b0cf5e4
Shubham Vilas Teke <62741544+shubhamteke45@users.noreply.github.com>
2021-07-02 09:56:13 +0530

    Create DetectLoopInLinkedList.java

---
+++ LinkedList_DSA/DetectLoopInLinkedList.java
@@ -0,0 +1,17 @@
+class Solution {
+    
+    public static boolean detectLoop(Node head){
+        Node fast = head.next;
+        Node slow = head;
+        
+        while(fast!=slow){
+            if(fast==null || fast.next==null){
+                return false;
+            }
+            
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+}
