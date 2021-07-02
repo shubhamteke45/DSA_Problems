7ac663cf5bb1a4650399125b04389f85519e3802
Shubham Vilas Teke <62741544+shubhamteke45@users.noreply.github.com>
2021-07-02 11:52:50 +0530

    Create MiddleElementInLinkedList.java

---
+++ LinkedList_DSA/MiddleElementInLinkedList.java
@@ -0,0 +1,16 @@
+class Solution
+{
+    int getMiddle(Node head)
+    {
+         Node fast = head;
+         Node slow = head;
+         
+         while(fast!=null && fast.next!=null){
+             fast = fast.next.next;
+             slow = slow.next;
+         }
+         
+         return slow.data;
+         
+    }
+}
