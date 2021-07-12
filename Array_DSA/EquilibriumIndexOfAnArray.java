7b4d8c9f79a7393e0dc653e7513085af18cbf84e
Shubham Vilas Teke <62741544+shubhamteke45@users.noreply.github.com>
2021-07-12 11:23:18 +0530

    Create EquilibriumIndexOfAnArray.java

---
+++ Array_DSA/EquilibriumIndexOfAnArray.java
@@ -0,0 +1,23 @@
+class GfG
+{
+           public static int findEquilibrium(int arr[], int n)
+            {
+                int sum = 0; 
+                int leftsum = 0; 
+ 
+        
+                for (int i = 0; i < n; ++i)
+                    sum += arr[i];
+ 
+                for (int i = 0; i < n; ++i) {
+                    sum -= arr[i];
+ 
+                if (leftsum == sum)
+                    return i;
+ 
+                leftsum += arr[i];
+            }
+ 
+                return -1;
+            }
+}
